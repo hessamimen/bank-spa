39e1bf206fec49ad463c2b00c4cb4365695ac5b9
Hessam Imen <hessam.imen@gmail.com>
2023-01-31 08:57:22 +1100

    feat: The Hero component is created and styled

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const GetStarted = () => {
-  return <div>GetStarted</div>;
-};
+//Assets
+import { arrowUp } from "../assets";
+
+const GetStarted = () => (
+  <div className="flex justify-center items-center w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer">
+    <div className="flex justify-center items-center flex-col bg-primary w-full h-full rounded-full">
+      <div className="flex justify-center items-start">
+        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-3">
+          <span className="text-gradient">Get</span>{" "}
+        </p>
+        <img
+          src={arrowUp}
+          alt="arrow"
+          className="w-[23px] h-[23px] object-contain"
+        />
+      </div>
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <span className="text-gradient">Started</span>
+      </p>
+    </div>
+  </div>
+);
 
 export default GetStarted;
