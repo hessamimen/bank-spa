7c05380413fc4a2dca048cb4ccbadb9b9f4c9589
Hessam Imen <hessam.imen@gmail.com>
2023-02-11 17:53:55 +1100

    feat: The CardDeal component is created and styled

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { card } from "../assets";
+import Button from "./Button";
 
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className="flex md:flex-row flex-col sm:px-16 px-6">
+      <div className="flex-1 flex justify-center items-start flex-col">
+        <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className="flex-1 flex justify-center items-center md:ml-10 ml-0 md:mt-0 mt-10 relative">
+        <img src={card} alt="card" className="w-full h-full" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
