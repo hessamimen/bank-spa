72cc3df0a8554fac0c362fe80eebde08995c9a93
Hessam Imen <hessam.imen@gmail.com>
2023-02-11 17:42:49 +1100

    feat: The Billing component is created and styled

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { apple, bill, google } from "../assets";
 
 const Billing = () => {
-  return <div>Billing</div>;
+  return (
+    <section
+      id="product"
+      className="flex md:flex-row flex-col-reverse sm:py-16 py-6"
+    >
+      <div className="flex-1 flex justify-center items-center md:mr-10 mr-0 md:mt-0 mt-10 relative">
+        <img
+          src={bill}
+          alt="billing"
+          className="w-full h-full relative z-[5]"
+        />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+      <div className="flex-1 flex justify-center items-start flex-col">
+        <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
+          Easily control your <br className="sm:block hidden" /> billing &
+          invoicing.
+        </h2>
+        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt="apple_store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google_play"
+            className="w-[128px] h-[42px] object-contain cursor-pointer"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Billing;
