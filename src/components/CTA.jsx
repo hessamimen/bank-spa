c6b5f4cec28e0b16c73b7c2aad5abc28ef5579f5
Hessam Imen <hessam.imen@gmail.com>
2023-02-11 18:56:06 +1100

    feat: The CTA component is created and styled

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Button from "./Button";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section className="flex flex-col sm:flex-row justify-center items-center sm:my-20 my-10 sm:px-16 px-6 sm:py-12 py-4 bg-black-gradient-2 rounded-[20px] box-shadow">
+      <div className="flex-1 flex flex-col">
+        <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
+          Let’s try our service now!
+        </h2>
+        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+      <div className="flex justify-center items-center sm:ml-10 ml-0 sm:mt-0 mt-10">
+        <Button />
+      </div>
+    </section>
+  );
 };
 
 export default CTA;
