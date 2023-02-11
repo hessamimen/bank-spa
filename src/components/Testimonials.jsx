d17a48a207d0bdf2de1871fca7ae2acfc904da29
Hessam Imen <hessam.imen@gmail.com>
2023-02-11 18:34:01 +1100

    feat: The Testimonial component is created and styled

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { feedback } from "../constants";
+import FeedbackCard from "./FeedbackCard";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section
+      id="clients"
+      className="sm:py-16 py-6 flex justify-center items-center flex-col relative"
+    >
+      <div className="absolute z-0 w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
+      <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+        <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
+          What people are <br className="sm:block hidden" /> saying about us
+        </h2>
+        <div className="w-full md:mt-0 mt-6">
+          <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-left max-w-[450px]">
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-center sm:justify-start w-full feedback-container relative z-[1]">
+        {feedback.map((card, index) => (
+          <FeedbackCard key={card.id} {...card} index={index} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
