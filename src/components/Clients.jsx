75a3a4a8805bcb09d883728fad1b3c46983f88c9
Hessam Imen <hessam.imen@gmail.com>
2023-02-11 18:42:26 +1100

    feat: The Clients component is created and styled

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { clients } from "../constants";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className="flex justify-center items-center my-4">
+      <div className="flex justify-center items-center flex-wrap w-full">
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className="flex-1 flex justify-center items-center sm:min-w-[192px] min-w-[120px]"
+          >
+            <img
+              src={client.logo}
+              alt="client_logo"
+              className="sm:w-[192px] w-[100px] object-contain "
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
